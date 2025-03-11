4514778b6fda48af41ef4f3da9815f840bff967b
tim chou <@TimChouSuperMicro>
2025-03-11 16:39:25 +0800

    Refactor: Move Class

--- kata/src/app/app.component.ts
+++ kata/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Roman } from './class';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,9 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'kata';
 
-  convert(romanNumeral: string) {
-    return 1;
+  convert(romanNumeral:string){
+    //Refactor: Move Class
+    const arabic = new Roman()
+    return arabic.convert(romanNumeral)
   }
 }
---
+++ kata/src/app/class.ts
@@ -0,0 +1,5 @@
+export class Roman {
+  convert(romanNumeral: string) {
+    return 1;
+  }
+}
