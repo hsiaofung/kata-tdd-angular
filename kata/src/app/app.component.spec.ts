c63dc14f14716fed5f35251dacdcab799d34a3ed
tim chou <@TimChouSuperMicro>
2025-03-11 16:15:35 +0800

    rename variable

--- kata/src/app/app.component.spec.ts
+++ kata/src/app/app.component.spec.ts
@@ -1,29 +1,26 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { convertToParamMap } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
-  });
+  });  
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+  //Rename Variable
+  it('convertsSingleDigitRoman', ()=>{
+    convertsSingleDigitRoman()
 
-  it(`should have the 'kata' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('kata');
-  });
+    function convertsSingleDigitRoman(){
+      const arabic = convert("I")
+      expect(arabic).toEqual(1)
+    }
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, kata');
-  });
+    function convert(romanNumeral:string){
+     return 1 
+    }
+  })
+  
 });
