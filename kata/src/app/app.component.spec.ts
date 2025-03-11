ba94468b5b4b70f9007f6bc31e73d60cf97c0583
tim chou <@TimChouSuperMicro>
2025-03-11 16:48:07 +0800

    refactor: inline code

--- kata/src/app/app.component.spec.ts
+++ kata/src/app/app.component.spec.ts
@@ -2,20 +2,20 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent - without TestBed', () => {
-  let component: AppComponent
+  let component: AppComponent;
 
-  beforeEach(()=>{
-    component = new AppComponent()
-  })
+  beforeEach(() => {
+    component = new AppComponent();
+  });
 
   //Rename Variable
-  it('convertsSingleDigitRoman', ()=>{
-    convertsSingleDigitRoman()
+  it('convertsSingleDigitRoman', () => {
+    convertsSingleDigitRoman();
 
-    function convertsSingleDigitRoman(){
-      const arabic = component.convert("I")
-      expect(arabic).toEqual(1)
-    }    
-  })
-  
+    function convertsSingleDigitRoman() {
+      expect(component.convert('I')).toEqual(1);
+      expect(component.convert('V')).toEqual(5);
+      expect(component.convert('X')).toEqual(10);
+    }
+  });
 });
--- kata/src/app/class.ts
+++ kata/src/app/class.ts
@@ -1,5 +1,11 @@
 export class Roman {
   convert(romanNumeral: string) {
-    return 1;
+    if (romanNumeral === 'I') {
+      return 1;
+    } else if (romanNumeral === 'V') {
+      return 5;
+    } else {
+      return 10;
+    }
   }
 }
