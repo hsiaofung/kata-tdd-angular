7c70ca372532e8395ab5f09c054e71184f4560d5
tim chou <@TimChouSuperMicro>
2025-03-12 13:17:59 +0800

    refactor: extract method

--- kata/src/app/class.ts
+++ kata/src/app/class.ts
@@ -1,17 +1,32 @@
 export class Roman {
   convert(romanNumeral: string) {
+    if (romanNumeral.length === 1) {
+      return this.convertsSingleDigit(romanNumeral);
+    } else {
+      if (romanNumeral === 'II') {
+        return 2;
+      } else if (romanNumeral === 'III') {
+        return 3;
+      } else {
+        return 6;
+      }
+    }
+  }
+  convertsSingleDigit(romanNumeral: string) {
     if (romanNumeral === 'I') {
       return 1;
     } else if (romanNumeral === 'V') {
       return 5;
-    } else if (romanNumeral === 'II') {
-      return 2;
-    } else if (romanNumeral === 'III') {
-      return 3;
-    } else if (romanNumeral === 'VI') {
-      return 6;
-    } else {
+    } else if (romanNumeral === 'X') {
       return 10;
+    } else if (romanNumeral === 'L') {
+      return 50;
+    } else if (romanNumeral === 'C') {
+      return 100;
+    } else if (romanNumeral === 'D') {
+      return 500;
+    } else {
+      return 1000;
     }
   }
 }
