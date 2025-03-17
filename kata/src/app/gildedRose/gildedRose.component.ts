d80e6cd68affa4cb1a42c26b075d691fec29a8fe
tim chou <@TimChouSuperMicro>
2025-03-17 10:54:35 +0800

    重構: 提取常數字串

--- kata/src/app/gildedRose/gildedRose.component.ts
+++ kata/src/app/gildedRose/gildedRose.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Item } from './type';
+import { Item, ItemName } from './type';
 
 @Component({
   selector: 'app-gildedRose',
@@ -10,20 +10,20 @@ export class GildedRoseComponent implements OnInit {
   @Input() items: Item[] = [];
   name = '';
   sellIn = 0;
-  quality = 0;
+  quality = 0;  
 
   constructor() {}
 
-  ngOnInit() {} 
+  ngOnInit() {}
 
-  updateQuality() {    
+  updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       if (
-        !(this.items[i].name === 'Aged Brie') &&
-        !(this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert')
+        !(this.items[i].name === ItemName.AGED_BRIE) &&
+        !(this.items[i].name === ItemName.BACKSTAGE)
       ) {
         if (this.items[i].quality > 0) {
-          if (!(this.items[i].name === 'Sulfuras, Hand of Ragnaros')) {
+          if (!(this.items[i].name === ItemName.SULFURAS)) {
             this.items[i].quality = this.items[i].quality - 1;
           }
         }
@@ -31,7 +31,9 @@ export class GildedRoseComponent implements OnInit {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
 
-          if (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert') {
+          if (
+            this.items[i].name === ItemName.BACKSTAGE
+          ) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1;
@@ -39,7 +41,7 @@ export class GildedRoseComponent implements OnInit {
             }
 
             if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {                
+              if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1;
               }
             }
@@ -47,20 +49,25 @@ export class GildedRoseComponent implements OnInit {
         }
       }
 
-      if (!(this.items[i].name === 'Sulfuras, Hand of Ragnaros')) {
+      if (!(this.items[i].name === ItemName.SULFURAS)) {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
 
       if (this.items[i].sellIn < 0) {
-        if (!(this.items[i].name === 'Aged Brie')) {
-          if (!(this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert')) {
+        if (!(this.items[i].name === ItemName.AGED_BRIE)) {
+          if (
+            !(
+              this.items[i].name === ItemName.BACKSTAGE
+            )
+          ) {
             if (this.items[i].quality > 0) {
-              if (!(this.items[i].name === 'Sulfuras, Hand of Ragnaros')) {
+              if (!(this.items[i].name === ItemName.SULFURAS)) {
                 this.items[i].quality = this.items[i].quality - 1;
               }
             }
           } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
+            this.items[i].quality =
+              this.items[i].quality - this.items[i].quality;
           }
         } else {
           if (this.items[i].quality < 50) {
--- kata/src/app/gildedRose/type.ts
+++ kata/src/app/gildedRose/type.ts
@@ -3,3 +3,9 @@ export interface Item {
   sellIn: number;
   quality: number;
 }
+
+export enum ItemName {
+  AGED_BRIE = 'Aged Brie',  
+  BACKSTAGE = 'Backstage passes to a TAFKAL80ETC concert' ,
+  SULFURAS = 'Sulfuras, Hand of Ragnaros'
+}
